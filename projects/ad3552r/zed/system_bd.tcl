abea93be4aa427ae68b4046792f6bce03acc16de
sergiu arpadi <sergiu.arpadi@analog.com>
2022-03-14 18:21:55 +0200

    ad3552r: Update file names

--- projects/ad3552r/zed/system_bd.tcl
+++ projects/ad3552r/zed/system_bd.tcl
@@ -1,6 +1,6 @@
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 source $ad_hdl_dir/projects/scripts/adi_pd.tcl
-source ../common/pulsar_adc_pmdz_bd.tcl
+source ../common/ad3552r_bd.tcl
 
 #system ID
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
